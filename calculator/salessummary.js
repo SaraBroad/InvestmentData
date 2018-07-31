42ba123a002be9d31ba7d2c80f58e1039bd1f117
Sara Broad <saraebroad@gmail.com>
2018-07-31 17:35:56 -0400

    parse to object

--- calculator/salessummary.js
+++ calculator/salessummary.js
@@ -1,24 +1,30 @@
 var fs = require('fs');
-// var csv = require('csv');
-// var parse = require('csv-parse');
-// var async = require('async');
 var csv = require('fast-csv');
 
-fs.createReadStream("../Data.csv")
-.pipe(csv())
+//parse Data.csv file
+
+
+var dataArray = [];
+
+//first I tried this and I can get the 
+var stream = fs.createReadStream("../Data.csv")
+csv.fromStream(stream, {headers : ["txn_date", "txn_type", "txn_shares", "txn_price", "fund", "investor", "sales_rep"]})
 .on('data', function(data){
-    console.log(data);
+   console.log(data);
+    // console.log(dataArray); 
 })
 .on('end', function(data){
-    console.log("Read finished");
+   console.log("Read finished");
 });
 
 
-var salesSummaryForBroker = function(sales_rep) {
-
-//function
+var salesSummaryForBroker = function(data, sales_rep) {
 
 return "";
 };
+
+salesSummaryForBroker();
+
+
 
 module.exports = salesSummaryForBroker;
